94ae0b2cc8d5ede52891a6fc585ca6bf06900205
morevq <72866082+Morevq@users.noreply.github.com>
2024-02-03 20:08:30 +0300

    add need files

--- litr_django/litr_django/settings.py
+++ litr_django/litr_django/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'App'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'litr_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'first/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
