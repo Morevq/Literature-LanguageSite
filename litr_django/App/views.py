b1be700cf5b135919a545f2549759c3c0757af56
morevq <72866082+Morevq@users.noreply.github.com>
2024-02-06 19:47:46 +0300

    add need files

--- litr_django/App/views.py
+++ litr_django/App/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    '''то, что мы возвращаем на фронт на глувную страницу,
+        тут это название страницы и заголовок,
+        но это можно поменять'''
+    context = {
+        'pagename': 'Lorem ipsum dolor sit amet'
+    }
+    return render(request, 'index.html', context) #отправка данных на фронт
